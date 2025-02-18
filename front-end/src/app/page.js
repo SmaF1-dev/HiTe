742147f7b15e8adf073ed54c6e297ac0a508ce42
L1tDeV <Nikita_Lyutyi@mail.ru>
2025-02-19 01:57:22 +0300

    Completed desktop-version of main page

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
